03aa965d7c01a1f7ad8dabbab61cf7f3abd892e6
Kevin Rico <krico315@gmail.com>
2022-10-12 14:53:27 -0600

    added if/else statements

--- excercises/app.js
+++ excercises/app.js
@@ -1,1 +1,12 @@
 console.log("hello world")
+
+let num = -1
+
+if (num > 0){
+    console.log("positive")
+  }  else if (num < 0) {
+    console.log("negative")
+ } else{
+    console.log("not negative or positive");
+ }
+
