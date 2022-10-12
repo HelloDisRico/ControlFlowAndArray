f0a2224faa040779facc9603ef8d35846a485dab
Kevin Rico <krico315@gmail.com>
2022-10-12 15:55:51 -0600

    added if/else excercise

--- excercises/app.js
+++ excercises/app.js
@@ -1,6 +1,6 @@
 console.log("hello world")
-
-let num = -1
+//Solution 1
+let num = -10
 
 if (num > 0){
     console.log("positive")
@@ -9,4 +9,31 @@ if (num > 0){
  } else{
     console.log("not negative or positive");
  }
+// Solution 2
+const  allowedAge = 19
+if(allowedAge>=18){
+    console.log("access granted")
+}else(allowedAge<18)
+{console.log("no")}
 
+//Nested If/Else Excercise
+var num2 = 200
+if(num2>=100){
+    console.log("positive and greater")}
+    else if(num2<=100)
+{console.log("positive but less")}
+else(num2<=0)
+{console.log("negative")
+}
+
+var Grade=90;
+    if(Grade>=90)
+    console.log("Grade A");
+    if(Grade>=80 && Grade<90)
+    console.log("Grade B");
+    if(Grade>=70 && Grade<80)
+    console.log("Grade C");
+    if(Grade>=55 && Grade<70)
+    console.log("Grade D");
+    if(Grade<55)
+    console.log("Grade F");
